ca101ca4d74142961a05cdb1b96024a4c929d5bf
Brad <bradley.bergstrom@oit.edu>
2018-01-13 12:52:48 -0800

    first

---
+++ Lab1_Bradley_Bergstrom/Lab1_Bradley_Bergstrom/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1_Bradley_Bergstrom.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult view1()
+        {
+            ViewBag.Message = "Hello there";
+            return View();
+        }
+    }
+}
---
+++ Lab1_Bradley_Bergstrom/Lab1_Bradley_Bergstrom/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1_Bradley_Bergstrom.Models
+{
+    public class Class1
+    {
+        int age = 50;
+    }
+}
