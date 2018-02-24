8a27890726befc99f68a5241548af08c06c210be
Brad <bradley.bergstrom@oit.edu>
2018-02-24 12:55:44 -0800

    first

---
+++ Lab6_Bradley_Bergstrom/Lab6_Bradley_Bergstrom/Scripts/app.js
@@ -0,0 +1,12 @@
+﻿function displayInfo() {
+
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var age = document.getElementById("age").value;
+
+    var summary = firstName + " " + lastName + " is " + age + " years old.";
+
+    document.getElementById("summary").innerText = summary;
+
+    return false;
+}
