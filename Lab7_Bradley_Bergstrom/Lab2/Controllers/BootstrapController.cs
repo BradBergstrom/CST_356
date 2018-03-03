8f45e64c8e0f51a1f5c6d20891d46f8c46ad9488
Brad <bradley.bergstrom@oit.edu>
2018-03-03 13:16:58 -0800

    First commit

---
+++ Lab7_Bradley_Bergstrom/Lab2/Controllers/BootstrapController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab2.Data.Entities;
+using Lab2.Data;
+
+
+namespace Lab2.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        // GET: Bootstrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            var users = InMemoryDatabase.Users;
+
+            return View(users);
+
+        }
+    }
+}
---
+++ Lab7_Bradley_Bergstrom/ReadMe.txt
@@ -0,0 +1,4 @@
+I dont know if I did part of the lab wrong,
+but for the list of users I added a new view in the bootstrap 
+controller that displays the users just as the lab specifies.
+- Bradley
